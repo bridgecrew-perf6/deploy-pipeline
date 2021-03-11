a6277dce90ae5ca72bd9f5085a55767e3eec69ac
Bryce Lowe <brycel@zillowgroup.com>
2021-03-10 21:00:47 -0800

    implement a jinja loader capable of doing path based file lookups (rather than the FileSystemLoader which looks in a known set of folders for a template file).

--- deploy_pipeline/pipeline/templates.py
+++ deploy_pipeline/pipeline/templates.py
@@ -1,14 +1,32 @@
 import os
-from jinja2 import Environment, FileSystemLoader, select_autoescape, Template
+from jinja2.environment import Environment, Template
+from jinja2.exceptions import TemplateNotFound
+from jinja2.loaders import BaseLoader
+from jinja2.utils import select_autoescape
 
 
 def get_template(template_path: str) -> Template:
-    directory, name = os.path.split(template_path)
-
     # init jinja environment
     return Environment(
-        loader=FileSystemLoader(directory),
+        loader=FullPathLoader(),
         autoescape=select_autoescape(['html', 'xml']),
         trim_blocks=True,
         lstrip_blocks=True
-    ).get_template(name)
+    ).get_template(template_path)
+
+
+# implement a loader that is capable of loading a template using an absolute path
+class FullPathLoader(BaseLoader):
+    def get_source(self, environment, template):
+        # ensure the file exists
+        if not os.path.exists(template):
+            raise TemplateNotFound(template)
+
+        # modified time
+        mtime = os.path.getmtime(template)
+
+        # open the file (in binary mode)
+        with open(template, "rb") as f:
+            source = f.read().decode('utf-8')
+
+        return source, template, lambda: mtime == os.path.getmtime(template)
